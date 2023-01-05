774f971ba3a7265df08f7316507d7e9b59f52ea7
Joerg Schneider <joerg.schneider@zeppelin.com>
2023-01-05 16:59:17 +0100

    feat(header-component): try packaging storybook component for usage

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,6 @@ import 'tailwindcss/tailwind.css';
 import Button from './components/Button.jsx';
 import Link from './components/Link.jsx';
 import CustomRadioGroup from './components/CustomRadioGroup.jsx';
+import {Header} from './stories/Header.jsx';
 
-export {Button, Link, CustomRadioGroup};
+export {Button, Link, CustomRadioGroup, Header};
--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -5,7 +5,7 @@ import { Button } from './Button';
 import './header.css';
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
-  <header>
+  <header className='bg-orange-400'>
     <div className="wrapper">
       <div>
         <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
