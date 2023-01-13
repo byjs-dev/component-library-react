d0a3ab68d391d3fd7f31a4dd3fcda5d1445013ec
Joerg Schneider <joerg.schneider@zeppelin.com>
2023-01-13 16:44:56 +0100

    chore(button-story): ts ignore

--- src/components/Button.stories.ts
+++ src/components/Button.stories.ts
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+// @ts-ignore
 const meta = {
   title: 'Components/Button',
   component: Button,
